9be5cf3766e7f64dfa11c4cb9326d18b587bd11a
Saniya <khanamayinas0304@gmail.com>
2024-12-21 18:42:08 +0100

    part 4 complete

--- src/mainrpg/GamePanelRPG.java
+++ src/mainrpg/GamePanelRPG.java
@@ -3,6 +3,7 @@ package mainrpg;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -18,22 +19,19 @@ public class GamePanelRPG extends JPanel implements Runnable{
          final int scale =  3;	 
          
          public final int tileSize = originalTileSize * scale; //48x48 tile
-         final int maxScreenCol = 16;
-         final int maxScreenRow = 12;
-         final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-         final int screenHeight = tileSize * maxScreenRow; //576 pixels
+         public final int maxScreenCol = 16;
+         public final int maxScreenRow = 12;
+         public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+         public final int screenHeight = tileSize * maxScreenRow; //576 pixels
          
          //FPS
          int FPS = 60;
          
+         TileManager tileM = new TileManager(this);
          KeyHandler keyH = new KeyHandler();
          Thread gameThread;
          Player player = new Player(this,keyH);
          
-         //set player's default position
-         int playerX = 100;
-         int playerY = 100;
-         int playerSpeed = 4;
          
          
          public  GamePanelRPG() {
@@ -122,6 +120,8 @@ public class GamePanelRPG extends JPanel implements Runnable{
         	 super.paintComponent(g);
         	 
         	 Graphics2D g2 = (Graphics2D)g;
+        	 
+        	 tileM.draw(g2);
         	 
         	 player.draw(g2);
         	 
