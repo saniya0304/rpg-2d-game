dc1e321093dd7de7d3ca841c6090da11cbd09931
Saniya <khanamayinas0304@gmail.com>
2024-12-25 18:02:29 +0100

    part 7 complete

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,9 +15,11 @@ public class Player extends Entity {
 	
 	GamePanel gp;
 	KeyHandler keyH;
-	
 	public final int screenX;
 	public final int screenY;
+	int hasKey = 0;
+	
+	
 	
 	public Player(GamePanel gamePanel, KeyHandler keyH) {
 		 
@@ -30,6 +32,8 @@ public class Player extends Entity {
 		solidArea = new Rectangle();
 		solidArea.x = 8;
 		solidArea.y = 16;
+		solidAreaDefaultX = solidArea.x;
+		solidAreaDefaultY = solidArea.y;
 		solidArea.width = 32;
 		solidArea.height = 32;
 		
@@ -85,6 +89,10 @@ public class Player extends Entity {
    	 collisionOn = false;
    	 gp.cChecker.checkTile(this);
    	 
+   	 // CHECK OBJECT COLLISION
+   	 int objIndex = gp.cChecker.checkObject(this, true);
+   	 pickUpObject(objIndex);
+   	 
    	 //IF COLLISION IS FALSE, PLAYER CAN MOVE
    	 if(collisionOn == false) {
    		 
@@ -125,6 +133,28 @@ public class Player extends Entity {
    	   }
    	  }
    }
+    public void pickUpObject(int i) {
+    	
+    	if(i != 999) {
+    	  
+    	   String objectName = gp.obj[i].name;
+    	   
+    	   switch(objectName) {
+    	   case "Key":
+    		  hasKey++;
+    		  gp.obj[i] = null;
+    		  System.out.println("Key:" + hasKey);
+    		  break;
+    	   case "Door":
+    		   if(hasKey > 0) {
+    			   gp.obj[i] = null;
+    			   hasKey--;
+    		   }
+    		   System.out.println("Key:" + hasKey);
+    		   break;
+    	   }
+    	}
+    }
     public void draw(Graphics2D g2) {
     	
 //    	g2.setColor(Color.white);
